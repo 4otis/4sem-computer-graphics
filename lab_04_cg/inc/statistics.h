fee83ceb0e2a316ab38e7972b7e794f92250accb
Bush <sdimmax@mail.ru>
2024-05-07 14:11:02 +0300

    statistic module was initially created.

---
+++ lab_04_cg/inc/statistics.h
@@ -0,0 +1,15 @@
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+#include "draw_algorithms.h"
+
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QtCharts>
+
+void show_time_bar_circles(QHBoxLayout *layout);
+void show_time_bar_ellipses(QHBoxLayout *layout);
+
+void get_stat_drawline_algorithms();
+
+#endif // STATISTICS_H

1be2ab183c550e67395294c5cece75d84d0ad22c
Bush <sdimmax@mail.ru>
2024-05-07 14:10:25 +0300

    Ellipses drawing methods were added.

--- lab_04_cg/inc/draw_algorithms.h
+++ lab_04_cg/inc/draw_algorithms.h
@@ -2,7 +2,7 @@
 #define DRAW_ALGORITHMS_H
 
 #include "my_circle.h"
-// #include "my_ellipse.h"
+#include "my_ellipse.h"
 #include <QList>
 
 struct my_point {
@@ -15,15 +15,15 @@ enum algorithms { CANONIC, PARAMETRIC, MIDPOINT, BRESENHAM, BUILT_IN };
 using algorithm_t = enum algorithms;
 
 QList<point_t> canonic_circle(circle_t &circle);
-// QList<point_t> canonic_ellipse(ellipse_t &ellipse);
+QList<point_t> canonic_ellipse(ellipse_t &ellipse);
 
 QList<point_t> param_circle(circle_t &circle);
-// QList<point_t> param_ellipse(ellipse_t &ellipse);
+QList<point_t> param_ellipse(ellipse_t &ellipse);
 
 QList<point_t> midpoint_circle(circle_t &circle);
-// QList<point_t> midpoint_ellipse(ellipse_t &ellipse);
+QList<point_t> midpoint_ellipse(ellipse_t &ellipse);
 
 QList<point_t> bresenham_circle(circle_t &circle);
-// QList<point_t> bresenham_ellipse(ellipse_t &ellipse);
+QList<point_t> bresenham_ellipse(ellipse_t &ellipse);
 
 #endif // DRAW_ALGORITHMS_H

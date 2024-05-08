50336b0e086fc9b1f6b7b02749f8734e9e6e9c34
Bush <sdimmax@mail.ru>
2024-05-08 19:49:42 +0300

    render funcs were added.

--- lab_05_cg/inc/rendering.h
+++ lab_05_cg/inc/rendering.h
@@ -1,4 +1,26 @@
 #ifndef RENDERING_H
 #define RENDERING_H
 
+#include <QDebug>
+#include <QGraphicsScene>
+#include <QPainter>
+#include <QPoint>
+
+#include "my_point.h"
+// #include "errorcodes.h"
+
+struct render {
+    QGraphicsScene *scene;
+    QPainter *p;
+    double width;
+    double height;
+};
+using render_t = struct render;
+
+error_t render_point(render_t &data, point_t &point, QColor color);
+error_t render_edge(render_t &data, point_t &p1, point_t &p2);
+error_t render_all_connecting_edges(render_t &data, points_t &points);
+
+// error_t render_fill(render_t &data, points_t &points);
+
 #endif // RENDERING_H
--- lab_05_cg/src/rendering.cpp
+++ lab_05_cg/src/rendering.cpp
@@ -1,1 +1,43 @@
 #include "rendering.h"
+
+error_t render_point(render_t &data, point_t &point, QColor color) {
+    error_t rc = SUCCESS;
+
+    QPixmap pixmap = QPixmap(data.width, data.height);
+    pixmap.fill(Qt::transparent);
+
+    QPainter painter(&pixmap);
+
+    data.p = &painter;
+    data.p->setPen(color);
+
+    data.p->drawPoint(point.x, point.y);
+
+    if (rc == SUCCESS)
+        data.scene->addPixmap(pixmap);
+
+    return rc;
+}
+
+error_t render_edge(render_t &data, point_t &p1, point_t &p2) {
+    error_t rc = SUCCESS;
+
+    QPixmap pixmap = QPixmap(data.width, data.height);
+    pixmap.fill(Qt::transparent);
+
+    QPainter painter(&pixmap);
+
+    data.p = &painter;
+    data.p->setPen(Qt::black);
+
+    data.p->drawLine(QPoint(p1.x, p1.y), QPoint(p2.x, p2.y));
+
+    if (rc == SUCCESS)
+        data.scene->addPixmap(pixmap);
+
+    return rc;
+}
+
+error_t render_all_connecting_edges(render_t &data, points_t &points) {
+    return render_edge(data, points.arr[0], points.arr[points.alen - 1]);
+}
